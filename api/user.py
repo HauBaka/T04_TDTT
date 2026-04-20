c173a132ceee11526d45c208e42bdbcd8535547a
Ponnef <baon46828@gmail.com>
2026-04-20 21:56:33 +0700

    [FIX] remove class Auth

--- api/user.py
+++ api/user.py
@@ -7,18 +7,6 @@ from schemas.response_schema import ResponseSchema
 
 user_router = APIRouter()
 
-class AuthService:
-    async def get_uid_from_token(self, token: str) -> str:
-        try:
-            decoded_info = auth.verify_id_token(token)
-            uid = decoded_info["uid"]
-            return uid
-        except auth.ExpiredIdTokenError:
-            raise AppException(status_code=401, message="Token đã hết hạn")
-        except auth.InvalidIdTokenError:
-            raise AppException(status_code=401, message="Token không hợp lệ")
-        except Exception as e:
-            raise AppException(status_code=401, message=f"Xác thực thất bại: {str(e)}")
             
 @user_router.get("/me", response_model=ResponseSchema)
 async def get_me(token: str):
