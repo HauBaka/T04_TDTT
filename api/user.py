db9bc70d378f1e97ec7cfff88644486987390510
Ponnef <baon46828@gmail.com>
2026-04-20 20:08:42 +0700

    Update user.py

--- api/user.py
+++ api/user.py
@@ -1,12 +1,25 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from core.exceptions import AppException
-from services.auth_service import AuthenticationService
+from firebase_admin import auth
 from services.user_service import user_service
-from schemas.user_schema import UserResponse, UserUpdateRequest
+from schemas.user_schema import UserUpdateRequest
 from schemas.response_schema import ResponseSchema
-from test_local import BaseResponse
+
 user_router = APIRouter()
 
+class AuthService:
+    async def get_uid_from_token(self, token: str) -> str:
+        try:
+            decoded_info = auth.verify_id_token(token)
+            uid = decoded_info["uid"]
+            return uid
+        except auth.ExpiredIdTokenError:
+            raise AppException(status_code=401, message="Token đã hết hạn")
+        except auth.InvalidIdTokenError:
+            raise AppException(status_code=401, message="Token không hợp lệ")
+        except Exception as e:
+            raise AppException(status_code=401, message=f"Xác thực thất bại: {str(e)}")
+            
 @user_router.get("/me", response_model=ResponseSchema)
 async def get_me(token: str):
     try:
