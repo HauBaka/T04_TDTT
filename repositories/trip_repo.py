cb3c64b71d47da1fde5d5027397ae0289b62ce62
Ponnef <baon46828@gmail.com>
2026-04-28 12:53:09 +0700

    [FIX] trip_repo.py and update subcollection

--- repositories/trip_repo.py
+++ repositories/trip_repo.py
@@ -16,7 +16,7 @@ class TripRepository(BaseRepository):
         "owner_uid": uid,
         "created_at": now,
         "updated_at": now,
-        "status": TripStatus.WAITING,
+        "status": TripStatus.WAITING.value,
         "member_uids": [uid]
     })
         trip_id = await self._create(payload)
@@ -33,7 +33,7 @@ class TripRepository(BaseRepository):
         
         if not payload:
             return await self.get_by_id(trip_id)
-        payload["update_at"] = datetime.now(timezone.utc)
+        payload["updated_at"] = datetime.now(timezone.utc)
         if "status" in payload and hasattr(payload["status"],"value"):
             payload["status"] = payload["status"].value
         #sd hàm base
@@ -57,7 +57,53 @@ class TripRepository(BaseRepository):
         }
         await self._update(trip_id, payload)
         return await self._get_by_id(trip_id)
-
+    async def add_member_to_subcollection(self,trip_id: str,uid: str,member_info: dict) -> dict | None:
+        "add member vào sub_collection"
+        db = self._get_db()
+        member_data = member_info.copy()
+        member_data.update({"uid": uid,"joined_at": datetime.now(timezone.utc)})
+        ref = db.collection("trips").document(trip_id).collection("members").document(uid)
+        await ref.set(member_data)
+        return member_data 
+    async def remove_member_from_subcollection(self, trip_id: str, uid: str) -> dict | None:
+        "Xóa member khỏi collection"
+        db = self._get_db()
+        ref = db.collection("trips").document(trip_id).collection("members").document(uid)
+        doc = await ref.get()
+        if not doc.exist:
+            return None
+        deleted_data = doc.to_dict()
+        deleted_data["uid"] = doc.id
+        await ref.delete()
+        return deleted_data
+    async def get_members(self, trip_id: str) -> list[dict]:
+        "lấy danh sách member từ collection"
+        if not trip_id:
+            return []
+        db = self._get_db()
+        docs = db.collection("trips").document(trip_id).collection("members").stream()
+        
+        members = []
+        async for doc in docs:
+            member_data = doc.to_dict()
+            if member_data:
+                member_data["uid"] = doc.id
+                members.append(member_data)
+        return members
+    async def update_member_in_subcollection(self,trip_id: str,uid: str,update_data: dict) -> dict | None:
+        "update member infor trong subcollection và return về thông tin mới nhất"
+        db = self._get_db()
+        payload = {k: v for k, v in update_data.items() if v is not None}
+        
+        ref = db.collection("trips").document(trip_id).collection("members").document(uid)
+        if payload:
+            await ref.update(payload)
+        doc = await ref.get()
+        if not doc.exist:
+            return None
+        member_data = doc.to_dict()
+        member_data["uid"] = doc.id
+        return member_data
     async def delete(self, trip_id: str) -> bool:
         """Xóa một trip."""
         return await self._delete(trip_id)
