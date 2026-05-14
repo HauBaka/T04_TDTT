627460010b3f379e0de35cb03ae8e426907ab38a
HauBaka <tuquynh7381@gmail.com>
2026-05-14 09:32:17 +0700

    [ADD] Quản lí favourite places

--- api/discover.py
+++ api/discover.py
@@ -1,13 +1,20 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, HTTPException
+
 from core.exceptions import AppException
-from schemas.discover_schema import AddressSuggestionRequest, AddressSuggestionResponse, DiscoverRequest
+from schemas.discover_schema import (
+    AddressSuggestionRequest,
+    AddressSuggestionResponse,
+    DiscoverRequest,
+)
 from schemas.response_schema import ResponseSchema
 from services.discover_service import DiscoverService
 
 discover_router = APIRouter()
 
+
 @discover_router.post("/discover", response_model=ResponseSchema)
 async def perform(payload: DiscoverRequest):
+    """Tìm lodgings"""
     try:
         discover_service = DiscoverService(payload)
         results = await discover_service.execute_discover_pipeline()
@@ -15,7 +22,11 @@ async def perform(payload: DiscoverRequest):
     except AppException as e:
         raise HTTPException(status_code=e.status_code, detail=e.message)
 
-# đề xuất, vd 5a nguyễn bỉnh khiêm -> 5a Đường Nguyễn Bỉnh Khiêm, Phường Đông Hòa, Thành Phố Dĩ An, Tỉnh Bình Dương
-@discover_router.post("/discover/address-suggest", response_model=ResponseSchema[AddressSuggestionResponse])
+
+@discover_router.post(
+    "/discover/address-suggest",
+    response_model=ResponseSchema[AddressSuggestionResponse],
+)
 async def suggest_address(query: AddressSuggestionRequest):
+    """Đề xuất địa chỉ dựa trên truy vấn đầu vào."""
     return await DiscoverService.suggest_addresses(query)
--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,11 +1,14 @@
-from pydantic import BaseModel, ConfigDict, Field
 from datetime import datetime
-from schemas.collection_schema import CollectionPrivateResponse, CollectionPublicResponse
-from schemas.user_preference_schema import ScoringWeights, UserBehaviorEvent, UserTravelPreference
+
+from pydantic import BaseModel, ConfigDict, Field
+
+from schemas.collection_schema import CollectionPublicResponse
+from schemas.user_preference_schema import ScoringWeights, UserTravelPreference
 
 
 class UserDocument(BaseModel):
     """users/{uid}"""
+
     model_config = ConfigDict(from_attributes=True)
 
     # Thông tin định danh
@@ -18,7 +21,7 @@ class UserDocument(BaseModel):
     avatar_url: str | None = None
     bio: str | None = Field(None, max_length=500)
     # Thông tin cá nhân (không hiển thị)
-    liked_collection: str
+    liked_collection: str  # Collection này dùng để lưu danh sách vị trí yêu thích, không xem là collection thực sự
     owned_collections: list[str] = Field(default_factory=list)
     contributing_collections: list[str] = Field(default_factory=list)
     saved_collections: list[str] = Field(default_factory=list)
@@ -36,12 +39,15 @@ class UserDocument(BaseModel):
     travel_profile: UserTravelPreference | None = None
     scoring_weights: ScoringWeights | None = None
 
+
 class SavedCollectionDocument(BaseModel):
     """Thông tin về collection đã được user lưu.
-        users/{uid}/saved_collections/{collection_id}
+    users/{uid}/saved_collections/{collection_id}
     """
+
     collection_id: str
     saved_at: datetime
+
 
 class UserCreateRequest(BaseModel):
     uid: str
@@ -71,8 +77,9 @@ class UserPublicResponse(BaseModel):
     display_name: str
     avatar_url: str | None = None
     bio: str | None = Field(None, max_length=500)
-    
+
     last_login: datetime | None = None
+
 
 class UserPrivateResponse(UserPublicResponse):
     email: str | None = None
@@ -85,14 +92,15 @@ class UserPrivateResponse(UserPublicResponse):
     last_updated: datetime | None = None
     last_login: datetime | None = None
 
-    current_trip: str | None = None 
+    current_trip: str | None = None
 
     travel_profile: UserTravelPreference | None = None
     scoring_weights: ScoringWeights | None = None
 
+
 class UserUpdateRequest(BaseModel):
     username: str | None = Field(None, min_length=3, max_length=16)
-    email: str | None = Field(None, pattern=r'^[\w\.-]+@[\w\.-]+\.\w+$')
+    email: str | None = Field(None, pattern=r"^[\w\.-]+@[\w\.-]+\.\w+$")
 
     display_name: str | None = Field(None, min_length=3, max_length=32)
     avatar_url: str | None = None
@@ -100,26 +108,38 @@ class UserUpdateRequest(BaseModel):
 
     phone_number: str | None = Field(None, max_length=10)
 
+
 class UserCollectionsResponse(BaseModel):
     liked: list[CollectionPublicResponse] = Field(default_factory=list)
     owned: list[CollectionPublicResponse] = Field(default_factory=list)
     collaborated: list[CollectionPublicResponse] = Field(default_factory=list)
 
+
 class UserSavedCollectionDocument(BaseModel):
     """users/{uid}/saved_collections/{collection_id}"""
+
     collection_id: str
     saved_at: datetime
 
+
 class UserContributingCollectionDocument(BaseModel):
     """users/{uid}/contributing_collections/{collection_id}"""
+
     collection_id: str
     contributed_count: int = 0
     joined_at: datetime
 
+
 class UserOwnedCollectionDocument(BaseModel):
     """users/{uid}/owned_collections/{collection_id}"""
+
     collection_id: str
     created_at: datetime
 
+
 class UserSaveCollectionRequest(BaseModel):
     collection_id: str
+
+
+class AddFavouritePlaceRequest(BaseModel):
+    place_id: str
